2560917f64bf420e0b48e5a6ed0f328c8613c899
Koryaga18 <paxwild.ns@gmail.com>
2024-02-24 14:32:35 +0600

    Add project files.

---
+++ Hotel.ATR.Portal/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Hotel.ATR.Portal.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Hotel.ATR.Portal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            User user = new User();
+            user.email ="ok@ok.kz";
+
+
+            _logger.LogError( "У пользователя {email} возникла ошибка {errorMessage} , " +
+                "{action} , {controller}" , user.email , "Ошибка пользователя" , "Index" , "Home" );
+
+            Stopwatch sw = new Stopwatch();
+            _logger.LogInformation("Обращаемся к сервису");
+
+            sw.Start();
+
+            Thread.Sleep(1000);
+            ///TODO обращение к чужому сервису
+            sw.Stop();
+
+            _logger.LogInformation("Сервис отработал {ElapsedMilliseconds}", sw.ElapsedMilliseconds);
+
+            _logger.LogInformation("logging Information");
+            _logger.LogCritical("Logging critical");
+            _logger.LogWarning("Logging LogWarning");
+            _logger.LogDebug("Logging LogDebug");
+
+
+         
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ Hotel.ATR.Portal/Controllers/RoomController.cs
@@ -0,0 +1,72 @@
+﻿using Hotel.ATR.Portal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.ATR.Portal.Controllers
+{
+    public class RoomController : Controller
+    {
+        private IWebHostEnvironment webHost;
+        private readonly ILogger<RoomController>_logger;
+
+
+        public IActionResult index(int id) {
+            _logger.LogInformation("logging Information");
+            _logger.LogCritical("Logging critical");
+            _logger.LogWarning("Logging LogWarning");
+            _logger.LogDebug("Logging LogDebug");
+            
+            ViewBag.Id = id;
+            TempData["id"] = id;
+
+            return View();
+        
+        }
+        public RoomController(IWebHostEnvironment webHost)
+        {
+            this.webHost = webHost;
+        }
+
+        public IActionResult Index(int page, int counter)
+        {
+
+            var user = new User() { email = "ok@ok.kz", name = "yevgenyi" };
+            ViewBag.User = user;
+            ViewData["user"] = user;
+            TempData["user"] = user;
+            return View(user);
+
+        }
+
+
+        public IActionResult RoomList()
+        {
+            return View();
+        }
+        public IActionResult RoomDetails()
+        {
+            return View();
+        }
+
+        /// <summary>
+        ///1.string email
+        ///2.User user
+        ///3.Request.Form
+        /// </summary>
+        [HttpPost]
+        public IActionResult SubscribeNewsLetter(IFormFile userFile)
+        {
+            var req = Request.Form["email"];
+
+            string path = Path.Combine(webHost.WebRootPath, userFile.FileName);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                userFile.CopyTo(stream);
+            }
+
+            //return View("Index");
+            return RedirectToAction("Index");
+            //return View("~/Views/Home/Index.cshtml");
+        }
+    }
+}
